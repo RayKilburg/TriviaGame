85040769ddf8a782fc47475aa59b34126da53015
RayKilburg <rkilburg42@gmail.com>
2018-06-22 00:10:55 -0400

    stillworkin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,136 @@
+// ////////////////////////////////////////////////////////
+// //start game btn
+// //hiding main game
+// $(".main-game").css("display", "none");
+// // $(".end-game").css("display", "block");
+// //start btn onclick
+// $(".start-button").on("click", function() {
+//   //displaying main game
+//   $(".main-game").css("display", "block");
+//   //hiding start button and background
+//   $(".start-button").css("display", "none");
+//   $(".start-game").css("display", "none");
+// });
+// //end-game onclick
+// // $(".end-button").on("click", function() {
+// // //   //displaying main game
+// //   $(".main-game").css("display", "none");
+// // //   //hiding start button and background
+// //   $(".end-button").css("display", "block");
+// //   $(".end-game").css("display", "block");
+// // });
+// // ".end-button".on("click", function() {
+// //   console.log(correct);
+// // });
+
+// //end game onlick
+
+// //question variables
+// function check() {
+//   var question1 = document.quiz.question1.value;
+//   var question2 = document.quiz.question2.value;
+//   var question3 = document.quiz.question3.value;
+//   var question4 = document.quiz.question4.value;
+//   var question5 = document.quiz.question5.value;
+//   var question6 = document.quiz.question6.value;
+//   var question7 = document.quiz.question7.value;
+//   var question8 = document.quiz.question8.value;
+//   var question9 = document.quiz.question9.value;
+//   var question10 = document.quiz.question10.value;
+//   var correct = 0;
+//   //logic for each question
+//   if (question1 == "yes") {
+//     correct++;
+//   }
+//   if (question2 == "yes") {
+//     correct++;
+//   }
+//   if (question3 == "yes") {
+//     correct++;
+//   }
+//   if (question4 == "yes") {
+//     correct++;
+//   }
+//   if (question5 == "yes") {
+//     correct++;
+//   }
+//   if (question6 == "yes") {
+//     correct++;
+//   }
+//   if (question7 == "yes") {
+//     correct++;
+//   }
+//   if (question8 == "yes") {
+//     correct++;
+//   }
+//   if (question9 == "yes") {
+//     correct++;
+//   }
+//   if (question10 == "yes") {
+//     correct++;
+//   }
+//   //na
+//   // var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
+//   // var messages = [
+//   //   "Great job!",
+//   //   "That's just okay",
+//   //   "You really need to do better"
+//   // ];
+//   //fix
+//   var score;
+
+//   if (correct == 0) {
+//     score = 2;
+//   }
+
+//   if (correct > 0 && correct < 3) {
+//     score = 1;
+//   }
+
+//   if (correct == 3) {
+//     score = 0;
+//   }
+//   //na/fix
+//   document.getElementById("after_submit").style.visibility = "visible";
+
+//   document.getElementById("message").innerHTML = messages[score];
+//   document.getElementById("number_correct").innerHTML =
+//     "You got " + correct + " correct.";
+//   document.getElementById("picture").src = pictures[score];
+//   // returning user score
+//   // document.getElementById("message").innerHTML = messages[correct];
+
+//   //90 sec countdown
+//   var timeLeft = 90;
+//   var elem = document.getElementById("some_div");
+//   var timerId = setInterval(countdown, 1000);
+
+//   function countdown() {
+//     if (timeLeft == -1) {
+//       clearTimeout(timerId);
+//       timesUp();
+//     } else {
+//       elem.innerHTML = timeLeft + " seconds remaining";
+//       timeLeft--;
+//     }
+//   }
+
+//   // function timesUp() {
+//   //   // alert("Times Up")
+// }
+
+// //timer function
+// //restart funtion btn
+// //fix score
+
+// //end game function
+// //if timeleft == -1
+
+// //start game function...starts on start game btn
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+//////////////////test///////////////////////////////
+///////////////////////////////////////////////////
 // var right;
 // var wrong;
 // var test;
