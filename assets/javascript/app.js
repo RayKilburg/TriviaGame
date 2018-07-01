3d8c876521afb33ff2352e305492034520325e0e
Ray <rkilburg42@gmail.com>
2018-07-01 17:14:51 -0400

    fixedbugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,16 @@
 ////////////////////////////////////////////////////////
 //start game btn
 //hiding main game
+var elem = document.getElementById("some_div");
+var timeLeft = 90;
+var timerid;
 $(".main-game").css("display", "none");
 $(".end-game").css("display", "none");
 //start btn onclick
 $(".start-button").on("click", function() {
-  var timerId = setInterval(countdown, 1000);
-  countdown();
+   timerId = setInterval(countdown, 1000);
+   $("#some_div").text("90 seconds remaining");
+  // countdown();
   //displaying main game
   $(".main-game").css("display", "block");
   //hiding start button and background
@@ -26,6 +30,8 @@ $(".submitBtn").on("click", function(event) {
 $(".end-button").on("click", function() {
   //   //displaying main game
   //   //hiding start button and background
+  clearInterval(timerId);
+  timeLeft = 90;
   $(".end-game").css("display", "none");
   $(".start-game").css("display", "block");
   $(".start-button").css("display", "block");
@@ -81,14 +87,14 @@ function check() {
   console.log("correct =" + correct);
 }
 
-//90 sec countdown
-var timeLeft = 90;
-var elem = document.getElementById("some_div");
+
 
 function countdown() {
   if (timeLeft == -1) {
-    clearTimeout(timerId);
-    timesUp();
+    clearInterval(timerId);
+    $(".main-game").css("display", "none");
+  $(".end-game").css("display", "block");
+  check();
   } else {
     elem.innerHTML = timeLeft + " seconds remaining";
 
@@ -102,4 +108,13 @@ function timesUp() {
   }
 }
 
-function reset() {}
+function reset() {
+  correct = 0;
+  timeLeft = 90;
+}
+
+//game end when timeLeft = -1
+//hide main
+//show end-game
+
+//stop clock when game is over reset back to 90 secs
